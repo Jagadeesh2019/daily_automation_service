993cc5cce9c447b7560056663ca13d546ca20d14
JagadeeshShopUp <jagadeesh@shop.co>
2020-08-19 00:21:58 +0530

    Added index.html

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,22 @@ const express = require("express");
 const router = express.Router();
 const http = require("http");
 const fs = require("fs");
+//const htmlData = require("./../index.html");
 
-router.get("/", (req, res) => {
-  res.json({ message: "This is Root1" });
+router.get("/index.html", (req, res) => {
+  //res.json({ message: "This is Root1" });
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  fs.readFile("./index.html", null, function (error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write("Whoops! File not found!");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 });
 
 module.exports = router;
